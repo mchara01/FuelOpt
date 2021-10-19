d1baa8fdd4cefbf70db2ce17ff1ada598143445a
Maxim Fishman <maxim.fishman@outlook.com>
2021-10-19 18:12:00 +0100

    change app title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Email and Password Login',
+      title: 'FuelOpt',
       theme: ThemeData(
         primaryColor: appColors.PrimaryBlue,
       ),
