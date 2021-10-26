be8ec0ed703a620fc3bd9c05a028694d58f80bd0
alicialawjy <alicialawjy@gmail.com>
2021-10-26 23:46:54 +0100

    Update Station model class for station info

--- backend/stations/models.py
+++ backend/stations/models.py
@@ -2,7 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Station(models.Model):
-    title = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    lat = models.DecimalField(decimal_places=7)
+    lng = models.DecimalField(decimal_places=7)
+    time = models.DateTimeField(auto_now_add=True) # Time added automatically
 
-    def __str__(self):
-        return self.title
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "location": self.location,
+            "latitude": self.lat,
+            "longitude":self.lng,
+            "time":self.time.strftime("%b %d %Y, %I:%M %p")
+            }
+    
