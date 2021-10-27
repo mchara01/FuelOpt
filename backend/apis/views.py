08ece56f25f5a62ae0264ce0eca0d2814c8e910a
nadimra <nadimrahman98@gmail.com>
2021-10-27 23:11:21 +0100

    database schema

--- backend/apis/views.py
+++ backend/apis/views.py
@@ -1,22 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics
-from django.db import connection
-cursor = connection.cursor()
 
 # Create your views here.
 from stations import models
 from .serializers import StationSerializer
 
 class ListStation(generics.ListCreateAPIView):
-
-    testquery = cursor.execute('SELECT * FROM stations_station')
-    print(testquery)
-    row = cursor.fetchone()
-    print(row)
-    row = cursor.fetchone()
-    print(row)
-
-
     queryset = models.Station.objects.all() 
     serializer_class = StationSerializer
 
--- backend/stations/models.py
+++ backend/stations/models.py
@@ -1,22 +1,48 @@
 from django.db import models
-from datetime import datetime;
+import datetime;
 
 # Create your models here.
 class Station(models.Model):
-    name = models.CharField(max_length=200)
-    location = models.CharField(max_length=200,default="Default")
+    name = models.CharField(max_length=200,default = "Default Name")
+    street = models.CharField(max_length=200,default="Default Street")
+    postcode = models.CharField(max_length=12,default="Default Postcode")
     lat = models.DecimalField(decimal_places=7, max_digits=10,default=0.0)
     lng = models.DecimalField(decimal_places=7, max_digits=10, default=0.0)
-    postcode = models.CharField(max_length=12,default="")
-    price = models.DecimalField(decimal_places=7, max_digits=10, default=0.0)
+    station_ref = models.IntegerField(default=0)
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
-            "location": self.location,
+            "street": self.street,
+            "postcode": self.postcode,
             "latitude": self.lat,
             "longitude":self.lng,
-            "postcode": self.postcode
+            "station_ref": self.station_ref,
             }
     
+class FuelPrice(models.Model):
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+
+    unleaded_price = models.DecimalField(decimal_places=2, max_digits=10,default=0.00)
+    diesel_price = models.DecimalField(decimal_places=2, max_digits=10,default=0.00)
+    super_unleaded_price = models.DecimalField(decimal_places=2, max_digits=10,default=0.00)
+    premium_unleaded_price = models.DecimalField(decimal_places=2, max_digits=10,default=0.00)
+
+    unleaded_date = models.DateField(default=datetime.date.today)
+    diesel_date = models.DateField(default=datetime.date.today)
+    super_unleaded_date = models.DateField(default=datetime.date.today)
+    premium_unleaded_date = models.DateField(default=datetime.date.today)
+
+    def serialize(self):
+        return {
+            "station": self.station.id,
+            "unleaded_price": self.unleaded_price,
+            "diesel_price": self.diesel_price,
+            "super_unleaded_price": self.super_unleaded_price,
+            "premium_unleaded_price": self.premium_unleaded_price,
+            "unleaded_date":self.unleaded_date,
+            "diesel_date": self.diesel_date,
+            "super_unleaded_date": self.super_unleaded_date,
+            "premium_unleaded_date": self.premium_unleaded_date,
+            }
