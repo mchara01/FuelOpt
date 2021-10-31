b9a94b41f10d6649df8f1f87a5527911e98946c8
Maxim Fishman <maxim.fishman@outlook.com>
2021-10-31 01:17:32 +0100

    extend stations data model fields

--- lib/model/stations_model.dart
+++ lib/model/stations_model.dart
@@ -1,13 +1,21 @@
 class Station {
   final int id;
   final String name;
+  final String location;
+  final double lat;
+  final double lng;
+  final String postcode;
 
-  Station({required this.id, required this.name});
+  Station({required this.id, required this.name, required this.location, required this.lat, required this.lng, required this.postcode,});
 
   factory Station.fromJson(Map<String, dynamic> json) {
     return Station(
       id: json['id'],
       name: json['name'],
+      location: json['location'],
+      lat: json['lat'],
+      lng: json['lng'],
+      postcode: json['postcode']
     );
   }
 }
