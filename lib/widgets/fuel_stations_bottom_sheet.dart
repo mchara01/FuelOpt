1d65e58a0abf4edc0d42855a34afebba865584e1
yeliu <yeliu0123@gmail.com>
2021-11-03 13:39:28 +0000

    add filter

--- lib/widgets/fuel_stations_bottom_sheet.dart
+++ lib/widgets/fuel_stations_bottom_sheet.dart
@@ -4,17 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:fuel_opt/widgets/search_bar.dart';
 import 'package:snapping_sheet/snapping_sheet.dart';
+import 'package:fuel_opt/widgets/filter1.dart';
 
 class FuelStationsBottomSheet extends StatefulWidget {
   const FuelStationsBottomSheet({Key? key}) : super(key: key);
 
   @override
-  _FuelStationsBottomSheetState createState() => _FuelStationsBottomSheetState();
+  _FuelStationsBottomSheetState createState() =>
+      _FuelStationsBottomSheetState();
 }
 
 class _FuelStationsBottomSheetState extends State<FuelStationsBottomSheet> {
-
-  final SnappingSheetController snappingSheetController = SnappingSheetController();
+  final SnappingSheetController snappingSheetController =
+      SnappingSheetController();
 
   late StreamSubscription<bool> keyboardSubscription;
 
@@ -30,7 +32,8 @@ class _FuelStationsBottomSheetState extends State<FuelStationsBottomSheet> {
 
     var keyboardVisibilityController = KeyboardVisibilityController();
 
-    keyboardSubscription = keyboardVisibilityController.onChange.listen((bool visible) {
+    keyboardSubscription =
+        keyboardVisibilityController.onChange.listen((bool visible) {
       isKeyboardVisible = visible;
     });
   }
@@ -46,24 +49,48 @@ class _FuelStationsBottomSheetState extends State<FuelStationsBottomSheet> {
             BoxShadow(blurRadius: 20.0, color: Colors.black.withOpacity(0.2))
           ],
         ),
-        child: SizedBox(height: 100, width: 100, child: SearchBar(searchOnTap: () {
-          programmaticBottomSheetMovement = true;
-          snappingSheetController.snapToPosition(const SnappingPosition.factor(positionFactor: 0.7)).then((value) {
-            bottomSheetPosition = snappingSheetController.currentPosition;
-            programmaticBottomSheetMovement = false;
-          });
-        },),),
+        child: SizedBox(
+          height: 100,
+          width: 100,
+          child: SearchBar(
+            searchOnTap: () {
+              programmaticBottomSheetMovement = true;
+              snappingSheetController
+                  .snapToPosition(
+                      const SnappingPosition.factor(positionFactor: 0.7))
+                  .then((value) {
+                bottomSheetPosition = snappingSheetController.currentPosition;
+                programmaticBottomSheetMovement = false;
+              });
+            },
+          ),
+        ),
       ),
       grabbingHeight: 80,
-      sheetBelow: SnappingSheetContent(child: Container(height: 100, width: 100, color: Colors.green), draggable: true),
-      initialSnappingPosition: const SnappingPosition.factor(positionFactor: 0.4),
+      sheetBelow: SnappingSheetContent(
+          child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.green,
+            child: Filter(),
+            // child: Column(
+            //   children: <Widget>[
+            //     Filter(),
+            //   ],
+            // ),
+          ),
+          draggable: true),
+      initialSnappingPosition:
+          const SnappingPosition.factor(positionFactor: 0.4),
       snappingPositions: const [
-        SnappingPosition.factor(positionFactor: 0.0, grabbingContentOffset: GrabbingContentOffset.top),
+        SnappingPosition.factor(
+            positionFactor: 0.0,
+            grabbingContentOffset: GrabbingContentOffset.top),
         SnappingPosition.factor(positionFactor: 0.4),
         SnappingPosition.factor(positionFactor: 0.7)
       ],
       onSheetMoved: (sheetPositionData) {
-        if(!programmaticBottomSheetMovement && isKeyboardVisible) {
+        if (!programmaticBottomSheetMovement && isKeyboardVisible) {
           if ((sheetPositionData.pixels - bottomSheetPosition!).abs() > 30) {
             FocusScope.of(context).unfocus();
           }
