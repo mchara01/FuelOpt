7108f1c6ab1e7ce3c4ade9a4ed47651226f63da7
WeiSin99 <looweisin008@gmail.com>
2021-12-29 14:20:06 +0000

    initial

--- backend/apis/tests.py
+++ backend/apis/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
+from .models import Station, FuelPrice, UserReview
+
 # Create your tests here.
+class ReviewTests(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_upload_by_receipt(self):
+        pass
+
+    def test_upload_manually_within_threshold(self):
+        pass
+
+    def test_upload_manually_outside_threshold(self):
+        pass
