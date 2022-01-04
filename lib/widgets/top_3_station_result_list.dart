47e14186950f4be858b3ff765c1b60ad732c2394
Maxim Fishman <maxim.fishman@outlook.com>
2022-01-04 02:31:29 -0800

    add station detail for top3 station results

--- lib/widgets/top_3_station_result_list.dart
+++ lib/widgets/top_3_station_result_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fuel_opt/model/fuelprice_model.dart';
 import 'package:fuel_opt/model/search_result.dart';
 import 'package:fuel_opt/model/top_3_station_result.dart';
+import 'package:fuel_opt/screens/stations_detail.dart';
 import '../model/filter_enums.dart';
 
 class Top3StationResultList extends StatelessWidget {
@@ -35,42 +36,49 @@ class Top3StationResultList extends StatelessWidget {
                 physics: const NeverScrollableScrollPhysics(),
                 itemBuilder: (context, index) {
                   StationResult stationResult = stationResults[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Flexible(
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(stationResult.name,
-                                      overflow: TextOverflow.ellipsis,),
-                                    const SizedBox(height: 5,),
-                                    Row(children: [
-                                      const Icon(Icons.location_pin),
-                                      Text(stationResult.street + ' · ' +
-                                          stationResult.distance!,)
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        return StationsDetail(stationResult);
+                      }));
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Flexible(
+                                child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(stationResult.name,
+                                        overflow: TextOverflow.ellipsis,),
+                                      const SizedBox(height: 5,),
+                                      Row(children: [
+                                        const Icon(Icons.location_pin),
+                                        Text(stationResult.street + ' · ' +
+                                            stationResult.distance!,)
+                                      ]),
+                                      const SizedBox(height: 5,),
+                                      Row(
+                                        children: [
+                                          const Icon(Icons.timer),
+                                          Text(stationResult.duration!),
+                                          const SizedBox(width: 5,),
+                                          const Icon(Icons.eco_outlined),
+                                          Text(stationResult.emission!)
+                                        ],
+                                      )
                                     ]),
-                                    const SizedBox(height: 5,),
-                                    Row(
-                                      children: [
-                                        const Icon(Icons.timer),
-                                        Text(stationResult.duration!),
-                                        const SizedBox(width: 5,),
-                                        const Icon(Icons.eco_outlined),
-                                        Text(stationResult.emission!)
-                                      ],
-                                    )
-                                  ]),
-                            ),
-                            Text(getPriceForPreference(
-                                stationResult.price, fuelTypePreference)
-                                .toString()),
-                          ],
+                              ),
+                              Text(getPriceForPreference(
+                                  stationResult.price, fuelTypePreference)
+                                  .toString()),
+                            ],
+                          ),
                         ),
                       ),
                     ),
